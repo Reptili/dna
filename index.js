de44dc7544e36ec5ac95f7dc1ca10070735d29ec
Reptili <seamusfu@gmail.com>
2021-07-31 21:03:25 -0700

    Added DNASeq

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log('hello world')
-console.log('sup')
+const fs = require('fs')
+const DNA = fs.readFileSync('./DNASeq.txt', {encoding: 'utf8', flag: 'r'})
+const marker = fs.readFileSync('./marker.txt', {encoding: 'utf8', flag: 'r'})
+
+console.log('Finds the appearances of different DNA markers in a DNA sequence.')
+console.log(DNA)
+console.log(marker)
+console.log(DNA.indexOf(marker))
