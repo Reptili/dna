6681f96d85aa99c75c4e5fec06e6be7e1273f580
Reptili <seamusfu@gmail.com>
2021-08-01 17:36:26 -0700

    index change

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const fs = require('fs')
-const DNA = fs.readFileSync('./DNASeq.txt', {encoding: 'utf8', flag: 'r'})
-const marker = fs.readFileSync('./marker.txt', {encoding: 'utf8', flag: 'r'})
-
 console.log('Finds the appearances of different DNA markers in a DNA sequence.')
-console.log(DNA)
-console.log(marker)
-console.log(DNA.indexOf(marker))
+
+const fs = require('fs')
+const markers = fs.readdirSync(__dirname + '/markers')
+const patients = fs.readdirSync(__dirname + '/patients')
+
+for (let i = 0; i < patients.length; i++) {
+    for (let z = 0; z < markers.length; z++) {
+        const m = fs.readFileSync('C:\\Users\\seamu\\dna/markers/' + markers[z], {encoding: 'utf8', flag: 'r'})
+        const p = fs.readFileSync('C:\\Users\\seamu\\dna/patients/' + patients[i], {encoding: 'utf8', flag: 'r'})
+        console.log(markers[z].split('.').slice(0, -1).join('.') + ': ' + p.indexOf(m)) 
+    }
+}
